008788b760e1d5ab4b8f6d02d53f7f88a1ede115
Brandon Nicholls <brandon.nicholls@gmail.com>
2015-05-08 13:53:42 -0600

    Use the babel require hook With the babel require hook we are able to use all of the ES6 features without being tied to a certain version of Node/IO. We also don't have to specify 'use strict'; The only exception is in app.js.  Because it is loaded prior to the require hook it can only rely on available ES6 features, thus I left the 'use strict'; directive to enable 'let'. If we change those 'let's to 'var's we can remove the 'use strict';

--- server/app.js
+++ server/app.js
@@ -1,20 +1,13 @@
 'use strict';
+require('babel/register');
 const koa  = require('koa');
 const koaStatic = require('koa-static');
 let app = koa();
 let router = new require('koa-router')();
 require('./controllers/financialEntity').init(router);
 
-
 app.use(koaStatic('./client'));
 app.use(router.routes());
-
-
-
-
-
-
-
 
 app.listen(8090);	
 console.log("Listening on port 8090");
