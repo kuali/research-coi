2d1ed802aab50ffc58dd3e3f9a8cbbe8e69845fd
Brandon Nicholls <brandon.nicholls@gmail.com>
2015-07-29 07:38:53 -0600

    Adding utf8

--- server/db/ConnectionManager.js
+++ server/db/ConnectionManager.js
@@ -6,7 +6,8 @@ let knexInstance = knex({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'coi'
+    database: process.env.DB_NAME || 'coi',
+    charset: 'utf8'
   },
   pool: {
     min: 0,
