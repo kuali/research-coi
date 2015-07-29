d9684e27b20b121da05806216e4eaae7e278032e
Brandon Nicholls <brandon.nicholls@gmail.com>
2015-07-29 00:19:59 -0600

    Connection manager with knex

---
+++ server/db/ConnectionManager.js
@@ -0,0 +1,19 @@
+import knex from 'knex';
+
+let knexInstance = knex({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'coi'
+  },
+  pool: {
+    min: 0,
+    max: process.env.CONNECTION_POOL_SIZE || 70
+  }
+});
+
+export default dbInfo => {
+  return knexInstance;
+};
