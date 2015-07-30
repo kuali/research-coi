b206f52098fc5b8757b43f1f9e147bf2a54ee005
Brandon Nicholls <brandon.nicholls@gmail.com>
2015-07-30 12:55:51 -0600

    Changing db scripts for deploys

---
+++ db/migration/create_schema.sql
@@ -0,0 +1,7 @@
+drop schema if exists monsters;
+CREATE SCHEMA IF NOT EXISTS monsters DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
+USE monsters;
+
+drop schema if exists gotham;
+CREATE SCHEMA IF NOT EXISTS gotham DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
+USE gotham;
