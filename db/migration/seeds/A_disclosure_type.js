835fa011c0c92bdfd231dc1db065798316301bc7
Brandon Nicholls <brandon.nicholls@gmail.com>
2015-09-09 18:52:54 -0600

    Ensuring seed file is run sequentially to avoid errors on deploy

--- db/migration/seeds/A_disclosure_type.js
+++
@@ -1,9 +0,0 @@
-/*eslint-disable camelcase */
-exports.seed = function(knex, Promise) {
-  console.log('Seed - disclosure_type');
-  return Promise.join(
-    knex('disclosure_type').insert({type_cd: 1, description: 'Manual'}),
-    knex('disclosure_type').insert({type_cd: 2, description: 'Annual'}),
-    knex('disclosure_type').insert({type_cd: 3, description: 'Project'})
-  );
-};
--- db/migration/seeds/A_relationship_status.js
+++
@@ -1,9 +0,0 @@
-/*eslint-disable camelcase */
-exports.seed = function(knex, Promise) {
-  console.log('Seed - relationship_status');
-  return Promise.join(
-    knex('relationship_status').insert({status_cd: 1, description: 'No Conflict'}),
-    knex('relationship_status').insert({status_cd: 2, description: 'Potential Relationship'}),
-    knex('relationship_status').insert({status_cd: 3, description: 'Managed Relationship'})
-  );
-};
