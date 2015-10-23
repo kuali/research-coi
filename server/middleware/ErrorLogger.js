c5a2669483748b53f6608232f2c50e8a095ed5c1
Brandon Nicholls <brandon.nicholls@gmail.com>
2015-10-22 22:02:09 -0600

    Improving error logger and adding a VERY basic 500 page

--- server/middleware/ErrorLogger.js
+++ server/middleware/ErrorLogger.js
@@ -2,5 +2,11 @@ import Log from '../Log';
 
 export default function(err, req, res, next) {
   Log.error(err.stack);
-  next(err);
+
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res.status(500);
+  res.send('<html><body>Error</body></html>');
 }
