b87e40b5cf7fed9fba170f01840aaa2586f62ea6
Joe Williams <joe.williams@kuali.co>
2016-02-08 19:24:34 -0600

    fix case of import

--- server/controllers/AdditionalReviewerController.js
+++ server/controllers/AdditionalReviewerController.js
@@ -15,7 +15,7 @@
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
-import * as AdditionalReviewerDB from '../db/AdditionalReviewerDB';
+import * as AdditionalReviewerDB from '../db/AdditionalReviewerDb';
 import Log from '../Log';
 import { COIConstants } from '../../COIConstants';
 import { OK, FORBIDDEN } from '../../HTTPStatusCodes';
