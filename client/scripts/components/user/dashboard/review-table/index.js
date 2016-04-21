12744963b13859191f21288f7904ea769627a7b8
Brandon Nicholls <brandon.nicholls@gmail.com>
2016-04-21 13:44:09 -0600

    Adds front end components for Review Table

---
+++ client/scripts/components/user/dashboard/review-table/index.js
@@ -0,0 +1,56 @@
+/*
+    The Conflict of Interest (COI) module of Kuali Research
+    Copyright © 2005-2016 Kuali, Inc.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+import styles from './style';
+import React from 'react';
+import {ReviewTableRow} from '../review-table-row';
+import ConfigStore from '../../../../stores/config-store';
+
+export function ReviewTable({disclosures}) {
+  let rows;
+  if (Array.isArray(disclosures)) {
+    rows = disclosures.map(disclosure => {
+      return (
+        <ReviewTableRow
+          key={disclosure.id}
+          reporter={disclosure.reporter}
+          type={ConfigStore.getDisclosureTypeString(disclosure.type)}
+          assignDate={disclosure.assign_date}
+          disclosureId={disclosure.id}
+        />
+      );
+    });
+  }
+
+  return (
+    <div role="grid" className={styles.container}>
+      <div role="row" className={styles.headings}>
+        <span role="columnheader" className={`${styles.heading} ${styles.columnOne}`}>
+          REPORTER NAME
+        </span>
+        <span role="columnheader" className={`${styles.heading} ${styles.columnTwo}`}>
+          DISCLOSURE TYPE
+        </span>
+        <span role="columnheader" className={`${styles.heading} ${styles.columnThree}`}>
+          REVIEW ASSIGNED
+        </span>
+      </div>
+      {rows}
+    </div>
+  );
+}
