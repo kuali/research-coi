d729546de4b18d0a97eb12bd337d3a5f9e197b6d
Brandon Nicholls <brandon.nicholls@gmail.com>
2016-10-06 14:40:33 -0600

    RESCOI-920: Allow removal of re-assigned reviewers

--- client/scripts/components/admin/detail-view/additional-reviewer/index.js
+++ client/scripts/components/admin/detail-view/additional-reviewer/index.js
@@ -18,67 +18,73 @@
 
 import styles from './style';
 import React from 'react';
-import { AdminActions } from '../../../../actions/admin-actions';
-import { formatDateTime } from '../../../../format-date';
+import {last} from 'lodash';
+import {AdminActions} from '../../../../actions/admin-actions';
+import {formatDateTime} from '../../../../format-date';
 import classNames from 'classnames';
-import { DATE_TYPE } from '../../../../../../coi-constants';
+import {DATE_TYPE} from '../../../../../../coi-constants';
 
 export default class AdditionalReviewer extends React.Component {
   constructor() {
     super();
 
     this.removeAdditionalReviewer = this.removeAdditionalReviewer.bind(this);
-    this.reassignAdditionalReviewer = this.reassignAdditionalReviewer.bind(this);
+    this.reassignReviewer = this.reassignReviewer.bind(this);
   }
 
   removeAdditionalReviewer() {
     AdminActions.removeAdditionalReviewer(this.props.id);
   }
 
-  reassignAdditionalReviewer() {
+  reassignReviewer() {
     AdminActions.reassignAdditionalReviewer(this.props.id);
   }
 
   render() {
-    const sortedDates = this.props.dates.sort((a,b) => {
+    const {props} = this;
+
+    const sortedDates = props.dates.sort((a, b) => {
       return new Date(a.date) - new Date(b.date);
     });
 
-    const dates = sortedDates.map((date,index) => {
-      const message = date.type === DATE_TYPE.COMPLETED ?
-        'Review Completed on ' :
-        'Reviewer Assigned on ';
+    const datesJsx = sortedDates.map((date, index) => {
+      let message;
+      if (date.type === DATE_TYPE.COMPLETED) {
+        message = 'Review Completed on';
+      }
+      else {
+        message = 'Reviewer Assigned on';
+      }
 
       return (
         <div key={index} className={styles.dates}>
-          {`${message}${formatDateTime(date.date)}`}
+          {`${message} ${formatDateTime(date.date)}`}
         </div>
       );
     });
 
-    const completedReview = sortedDates[sortedDates.length - 1].type === DATE_TYPE.COMPLETED;
-    const completedReviewOnce = sortedDates.some(date => date.type === DATE_TYPE.COMPLETED);
+    const completedReview = last(sortedDates).type === DATE_TYPE.COMPLETED;
     let removeButton;
-    if (!completedReviewOnce && !this.props.readOnly) {
+    if (!completedReview && !props.readOnly) {
       removeButton = (
         <button
           id="remove_button"
           className={styles.button}
           onClick={this.removeAdditionalReviewer}
         >
-          <i className={'fa fa-times'} style={{marginRight: '5px'}} />
+          <i className={'fa fa-times'} style={{marginRight: 5}} />
           Remove Reviewer
         </button>
       );
     }
 
     let reassignButton;
-    if (completedReview && !this.props.readOnly) {
+    if (completedReview && !props.readOnly) {
       reassignButton = (
         <button
           id="reassign_button"
           className={styles.button}
-          onClick={this.reassignAdditionalReviewer}
+          onClick={this.reassignReviewer}
         >
           Reassign
         </button>
@@ -92,24 +98,25 @@ export default class AdditionalReviewer extends React.Component {
       );
     }
 
-    const nameClasses = classNames(styles.name,
-      {[styles.check]: completedReview});
+    const nameClasses = classNames(
+      styles.name,
+      {[styles.check]: completedReview}
+    );
 
     return (
       <div className={styles.container}>
         <div className={styles.content}>
-
           <div className={nameClasses}>
-            {check}{this.props.name}
+            {check}{props.name}
           </div>
           <div className={styles.email}>
-            {this.props.email}
+            {props.email}
           </div>
         </div>
         <div className={styles.buttons}>
-          <div style={{display: 'inline-block'}}>
-            {dates}
-          </div>
+          <span>
+            {datesJsx}
+          </span>
 
           {removeButton}
           {reassignButton}
@@ -118,3 +125,16 @@ export default class AdditionalReviewer extends React.Component {
     );
   }
 }
+
+AdditionalReviewer.propTypes = {
+  id: React.PropTypes.number.isRequired,
+  dates: React.PropTypes.array.isRequired,
+  readOnly: React.PropTypes.bool.isRequired,
+  name: React.PropTypes.string.isRequired,
+  email: React.PropTypes.string
+};
+
+AdditionalReviewer.defaultProps = {
+  dates: [],
+  readOnly: false
+};
